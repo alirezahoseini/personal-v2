76bcbd17606fb8ce11bc584047aeb34f96044e12
Alirea Hosseini <mehdimj0161@gmail.com>
2023-03-10 17:50:51 +0330

    Add weather app to projects

--- projects/weather/src/js/dom.js
+++ projects/weather/src/js/dom.js
@@ -1,7 +1,9 @@
+import GetApis from "./getApis.js";
 import Theme from "./theme.js";
 import WeatherApp from "./weatherApp.js";
 const theme = new Theme();
 const weatherApp = new WeatherApp();
+const getApis = new GetApis();
 
 
 class Dom {
@@ -15,33 +17,19 @@ class Dom {
     const selectedElement = document.querySelector(elementId);
     selectedElement.classList.remove(classValue);
   }
-
   addClassTimeOut(elementId, time, classValue) {
     const selectedElement = document.querySelector(elementId);
     setTimeout(() => {
       selectedElement.classList.add(classValue);
     }, time);
   }
-
   addClass(elementId, classValue) {
     const selectedElement = document.querySelector(elementId);
     selectedElement.classList.add(classValue);
   }
 
-  replaceClass(element, prevClass, newClass) {
-    const selectedElem = document.querySelector(element);
-    selectedElem.classList.remove(prevClass);
-
-    setTimeout(() => {
-      selectedElem.classList.add(newClass);
-    }, 10);
-  }
   // Show APP Page
   async showApp() {
-    // Set 10s timeout for showing vpn error
-    const showErorTimeOut = setTimeout(() => {
-      this.showVpnError();
-    }, 20000);
     // show app page
     this.removeClassTimeOut("#app", 300, "hidden");
     // set theme
@@ -51,35 +39,27 @@ class Dom {
     // hidde first select city page
     this.addClass("#select_first_city", "hidden");
 
+    //---- Run first city
     // Access to user city from LS
     const userCityName = localStorage.getItem('userCity').toLowerCase();
-    // created url
-    const url = `https://key48798231.herokuapp.com/weather?input=${userCityName}`;
-    // send request
-    const request = await fetch(url).then((res) => res)
-    .catch((error) => {
-      this.showVpnError()
-      console.log(error)
-    })
-    // access response
-    const response = await request.json();
-
     // check response error
-    if(request.status === 200){
+    try {
+      // access response
+      const response = await getApis.getWeather(userCityName);
       this.runApp(response)
       this.addClassTimeOut("#select_first_city", 100, "hidden");
-      clearTimeout(showErorTimeOut)
-    }else{
+    } catch (error) {
       this.showVpnError()
     }
 
-      // // For test offline -------------
-      // const data = JSON.parse(localStorage.getItem('weather'));
-      // this.runApp(data)
-      // this.addClassTimeOut("#loading", 300, "hidde");
-      // this.removeClassTimeOut("#app", 100, "hidden");
-      // this.addClassTimeOut("#select_first_city", 100, "hidden");
-      // theme.firstLoadSetTheme()
+    //---- Run seconde city
+    // Access to seconde city from LS
+    const secondeCity = localStorage.getItem('secondeCity');
+    if(secondeCity !== null){
+      weatherApp.runSecondeCity()
+    }else{
+      this.removeClass('#add-city-button', 'hidden')
+    }
 
   }
   // Show First page and select city
@@ -89,7 +69,7 @@ class Dom {
     this.removeClass("#select_first_city", "hidden");
   }
   // Show Message
-  showMessage(message="default message", icon= "info", colorClass='success') {
+  showMessage(message = "default message", icon = "info", colorClass = 'success') {
     // access message box
     const messageBox = document.querySelector("#message_box");
     // create message tag
@@ -108,53 +88,65 @@ class Dom {
     messageTag.classList.remove('invisible')
     // slide on
     setTimeout(() => {
-        messageTag.classList.remove('animate_bottom')
+      messageTag.classList.remove('animate_bottom')
     }, 100);
     // slide out
     setTimeout(() => {
-        messageTag.classList.add('animate_top')
+      messageTag.classList.add('animate_top')
     }, 5000);
     // romowe from DOM
     setTimeout(() => {
-        messageTag.remove()
+      messageTag.remove()
     }, 5500);
   }
   // Run and create app
-  runApp(data){
-    console.log(data)
+  runApp(data) {
     localStorage.setItem('weather', JSON.stringify(data));
-    weatherApp.setRealtimeWeather(data[0]);
-    weatherApp.nextHours(data);
     weatherApp.setDateAndTime();
 
+
+    // Access to the weather data
+    const weatherData = data.list;
+
+    weatherApp.setRealtimeWeather(weatherData[0]);
+    weatherApp.nextHours(weatherData);
+
     // Remove loading frame classes
-    this.removeLoadingFrame('main_weather');
-    this.removeLoadingFrame('more_data_section');
-    this.removeLoadingFrame('next_hours_weather');
-    
+    weatherApp.removeLoadingFrame('main_weather');
+    weatherApp.removeLoadingFrame('more_data_section');
   }
   // Background hiddden Closer popups
-  backgroundHidden(){
+  backgroundHidden() {
     const bg = document.querySelector('#hidden_background_section');
     // access to elements for closeing
     const app = document.querySelector('#app'),
-          themeSwitcherMenu = document.querySelector('#switcher_menu');
+      themeSwitcherMenu = document.querySelector('#switcher_menu'),
+      changeCityPopup = document.querySelector('#change_city_popup'),
+      changeCityForm = document.querySelector('#change_city_form')
     bg.addEventListener('click', () => {
       // Close Theme Switcher
-      if(themeSwitcherMenu.classList.contains('active')){
+      if (themeSwitcherMenu.classList.contains('active')) {
         app.classList.remove('blur');
         themeSwitcherMenu.classList.remove('active');
         bg.classList.remove('active')
+      } else if (changeCityPopup.classList.contains('active')) {
+        app.classList.remove('blur');
+        changeCityPopup.classList.remove('active');
+        bg.classList.remove('active')
+      } else if (changeCityForm.classList.contains('active')) {
+        changeCityForm.classList.remove('active');
+        changeCityPopup.classList.add('active')
+        this.resetChangeCityForm()
       }
     })
   }
   // Show Vpn error
-  showVpnError(){
+  showVpnError() {
     // access to the Elements
     const appElem = document.querySelector('#app'),
-          selectFirstCityElem = document.querySelector('#select_first_city'),
-          vpnErrorElem = document.querySelector('#vpn_error'),
-          errorTextBox = vpnErrorElem.querySelector('#error-text');
+      selectFirstCityElem = document.querySelector('#select_first_city'),
+      vpnErrorElem = document.querySelector('#vpn_error'),
+      errorTextBox = vpnErrorElem.querySelector('#error-text');
 
     // Blur bg
     appElem.classList.add('blur');
@@ -166,14 +158,105 @@ class Dom {
       errorTextBox.classList.remove('animate_bottom')
     }, 500);
   }
-  // Remove loading frame
-  removeLoadingFrame(elementId){
-    // access to the loading classes
-    const loadingClasses = document.querySelectorAll(`#${elementId} .loading_frame`);
-    // remove classes
-    loadingClasses.forEach(currentElement => currentElement.classList.remove('loading_frame'))
+  /*---------------------------
+   Chnage city Popup and form 
+  ---------------------------*/
+  openChangeCityPopup() {
+    // Access to the Elements
+    const changeCityPopup = document.querySelector('#change_city_popup'),
+      backgroundSection = document.querySelector('#hidden_background_section'),
+      app = document.querySelector('#app');
+
+      // Open it
+    backgroundSection.classList.add('active')
+    app.classList.add('blur');
+    changeCityPopup.classList.add('active');
   }
-  
+  openChangeFirstCityForm() {
+    // Access to Elements
+    const form = document.querySelector('#change_city_form'),
+      popup = document.querySelector('#change_city_popup'),
+      title = form.querySelector('.title');
+
+    // Open it
+    form.classList.add('active')
+    popup.classList.remove('active')
+    title.children[0].classList.add('indigo')
+    title.children[1].innerHTML = 'Change your city';
+
+    // Set Form Status
+    form.setAttribute('form-status', 'firstCity')
+  }
+  openChangeSecondeCityForm() {
+    // Access to Elements
+    const form = document.querySelector('#change_city_form'),
+      popup = document.querySelector('#change_city_popup'),
+      title = form.querySelector('.title');
+
+    // Open it
+    form.classList.add('active')
+    popup.classList.remove('active')
+    title.children[0].classList.add('purple')
+    title.children[1].innerHTML = 'Add or change seconde city';
+
+    // Set Form Status
+    form.setAttribute('form-status', 'secondeCity')
+  }
+  closeChangeCityPopup() {
+    // Access to Elements
+    const changeCityPopup = document.querySelector('#change_city_popup'),
+      backgroundSection = document.querySelector('#hidden_background_section'),
+      app = document.querySelector('#app');
+    // close it
+    backgroundSection.classList.remove('active')
+    app.classList.remove('blur');
+    changeCityPopup.classList.remove('active')
+  }
+  closeChangeCityForm() {
+    // Access to Elements
+    const changeCityPopup = document.querySelector('#change_city_popup'),
+      changeCityForm = document.querySelector('#change_city_form');
+
+    // close it
+    changeCityPopup.classList.add('active')
+    changeCityForm.classList.remove('active')
+  }
+  resetChangeCityForm() {
+    const formElement = document.querySelector('#change_city_form'),
+      customCityInput = formElement.querySelector('#custom-city'),
+      defaultCiteisInput = formElement.querySelector('#default-cities'),
+      title = formElement.querySelector('.title'),
+      submitBtn = document.querySelector('#change_city_form #submit-form');
+
+
+    customCityInput.value = '';
+    defaultCiteisInput.value = 'null';
+    formElement.setAttribute('form-status', 'null');
+    submitBtn.classList.remove('active');
+    submitBtn.setAttribute('disabled', true);
+
+    if (title.children[0].classList.contains('purple')) {
+      title.children[0].classList.remove('purple')
+    } else if (title.children[0].classList.contains('indigo')) {
+      title.children[0].classList.remove('indigo')
+    }
+  }
+  changeCityFormValidation(value){
+    // create regEx Pattern 
+    const pattern = /\s|[1-9]|[!@#$%^&.*()_+]/g;
+
+    // validation
+    if(value.length < 3){
+      return false
+    }else if(value.match(pattern)){
+       return false;
+    }else{
+       return true;
+    }
+  }
+
+
+
 }
 
 export default Dom;
--- projects/weather/src/js/weatherApp.js
+++ projects/weather/src/js/weatherApp.js
@@ -1,6 +1,8 @@
+import GetApis from "./getApis.js";
+const getApis = new GetApis();
 class WeatherApp {
     // Set Realtime Weather
-    setRealtimeWeather(weatherData){
+    async setRealtimeWeather(weatherData) {
         // Set user City
         const userCity = localStorage.getItem('userCity');
         const userCityTag = document.querySelector('#realtime_weather .location')
@@ -8,174 +10,148 @@ class WeatherApp {
 
         // Set description
         const descriptionTag = document.querySelector('#realtime_weather .description')
-        descriptionTag.innerHTML =  weatherData.weather_description;
+        descriptionTag.innerHTML = weatherData.weather[0].description;
 
         // Set weather temperature
         const tempTag = document.querySelector('#realtime_weather .temp')
-        tempTag.innerHTML = Math.floor(weatherData.temp_now);
+        tempTag.innerHTML = Math.floor(weatherData.main.temp);
 
         // Set weather icon
         const weatherImgTag = document.querySelector('#realtime_weather .real-img img')
-        const date = new Date();
-        const hours = date.getHours();
-        if(hours > 19 || hours < 6){
-            weatherImgTag.src = weatherData.night_icon;
-        }else{
-            weatherImgTag.src = weatherData.day_icon;
-        }
-        
+        weatherImgTag.src = await getApis.findIcon(weatherData);
+
         // Set More info ------- access to elements
         const moreInfo = document.querySelector('#more_data_section'),
-              maxTemp = moreInfo.querySelector('#max_temp h4'),
-              minTemp = moreInfo.querySelector('#min_temp h4'),
-              humidity = moreInfo.querySelector('#humidity h4'),
-              wind = moreInfo.querySelector('#wind h4');
+            maxTempTag = moreInfo.querySelector('#max_temp h4'),
+            minTempTag = moreInfo.querySelector('#min_temp h4'),
+            humidityTag = moreInfo.querySelector('#humidity h4'),
+            windTag = moreInfo.querySelector('#wind h4');
 
-        
-        maxTemp.innerHTML = Math.round(weatherData.temp_max) + ' °';
-        minTemp.innerHTML = Math.round(weatherData.temp_min) + ' °';
-        humidity.innerHTML = weatherData.humidity;
-        wind.innerHTML = weatherData.wind;
 
-        // calculate Times
-        let timezone = weatherData.timezone;
-        let sunrise = weatherData.sunrise;
-        let sunset = weatherData.sunset;
-        // convert timezone
-        let sunriseTime = moment.utc(sunrise,'X').add(timezone,'seconds').format('HH:mm');
-        let sunsetTime = moment.utc(sunset,'X').add(timezone,'seconds').format('HH:mm');
+
+        maxTempTag.innerHTML = weatherData.main.temp_max + ' °';
+        minTempTag.innerHTML = weatherData.main.temp_min + ' °';
+        humidityTag.innerHTML = weatherData.main.humidity;
+        windTag.innerHTML = weatherData.wind.speed;
 
     }
     // Next hours Weather 
-    nextHours(weatherData){
-        // access to the time 
-        const date = new Date();
-        const hours = date.getHours();
+    async nextHours(weatherData) {
+        /* -------------------------------
+         Get time and find current hour
+        ------------------------------- */
+        let cityHour = await getApis.getTime();
+        cityHour = cityHour.watch.hour;
+
         // access to the slider static childs
         const sliderChildes = document.querySelectorAll('.nextHours .swiper-slide');
-        
+
+        // access to the current hour in weather data
+        let currentHour = '';
+        for (let index = 0; index < weatherData.length; index++) {
+            /// access to the weather hours
+            let time = (weatherData[index].dt_txt).split(" ")[1];
+            time = time.slice(0, 2);
+
+            // console.log(time)
+            if (time === cityHour) {
+                currentHour = index;
+                break
+            }
+        }
+
+        // cuting finally items from weather data array
+        const finallyItems = weatherData.slice(currentHour, currentHour + 7);
+
+        // Set to day weather to local storage
+        localStorage.setItem('toDayWeather', JSON.stringify(finallyItems))
+
         // Create next hours weathers in slider
-        weatherData.forEach((weather, index) => {
+        finallyItems.forEach(async (weather, index) => {
+            // access to te time 
+            let time = (weather.dt_txt).split(" ")[1];
+            time = time.slice(0, 2);
+
             // access slide element
             const slideElement = sliderChildes[index];
-            /// access to the weather hours
-            let time = (weather.date).split(" ")[1];
-            time = time.slice(0,5);
-            // set icon by any hours
-            let icon = '';
-            switch (time) {
-                case "18:00":
-                    icon = icon = weather.day_icon;;
-                    break;
-                case "21:00":
-                    icon = icon = weather.night_icon;;
-                    break;
-                case "00:00":
-                    icon = icon = weather.night_icon;;
-                    break;
-                case "03:00":
-                    icon = icon = weather.night_icon;;
-                    break;
-                case "06:00":
-                    icon = icon = weather.day_icon;;
-                    break;
-                case "09:00":
-                    icon = icon = weather.day_icon;;
-                    break;
-                case "12:00":
-                    icon = icon = weather.day_icon;;
-                    break;
-                case "15:00":
-                    icon = icon = weather.day_icon;;
-                    break;
-                default:
-                    icon = icon = weather.day_icon;;
-                    break;
-            }
-
-            /// Access to current time
-            let currentTime = '';
-            if(hours >= 18 && hours < 21){
-                currentTime = '18:00';
-            }else if(hours >= 21 && hours < 24){
-                currentTime = '21:00';
-            }else if(hours >= 24 || hours < 3){
-                currentTime = "00:00"
-            }else if(hours >= 3 && hours < 6){
-                currentTime = '03:00'
-            }else if(hours >= 6 && hours < 9){
-                currentTime = '06:00'
-            }else if(hours >= 9 && hours < 12){
-                currentTime = '09:00'
-            }else if(hours >= 12 && hours < 15){
-                currentTime = '12:00'
-            }else if(hours >= 15 && hours < 18){
-                currentTime = '15:00'
-            }
-
+            // access to tags
             const weatherInfo = slideElement.querySelector('.weather_info'),
-                  weatherIcon = slideElement.querySelector('.weather_icon'),
-                  weatherTime = slideElement.querySelector('.weather_time');
+                weatherIcon = slideElement.querySelector('.weather_icon'),
+                weatherTime = slideElement.querySelector('.weather_time');
+            // set data
+            weatherInfo.innerHTML = weather.weather[0].main;
+            weatherIcon.src = await getApis.findIcon(weather);
+            weatherTime.innerHTML = time + ':00';
+            slideElement.setAttribute('timeId', index)
 
-            weatherInfo.innerHTML = weather.weather;
-            weatherIcon.src = icon;
-            weatherTime.innerHTML = time;
-
-
-            if(currentTime === time){
+            // active current time
+            if (index == 0) {
                 slideElement.classList.add('active');
             }
 
-
+            // switching in times
+            slideElement.addEventListener('click', () => this.activeThisTime(index));
         });
-        
+
+        // Remove loading frame on next hour
+        this.removeLoadingFrame('nextHours');
+        for (let index = 0; index <= 35; index++) {
+            const weather = weatherData[index]
+            // access slide element
+            const slideElement = sliderChildes[index];
+            const icon = await getApis.findIcon(weather);
+        }
+    }
     
-    
+    //
+    activeThisTime(selectedTime) {
+        // access to to day weather from local storage
+        const weatherData = JSON.parse(localStorage.getItem('toDayWeather'))
+        // Active time
+        function run() {
+            // access to the time slides
+            const timeSlides = document.querySelectorAll('.nextHours .swiper-slide');
+            // unActive last slide
+            timeSlides.forEach((slide, index) => {
+                if (slide.classList.contains('active')) { slide.classList.remove('active') }
+            });
+            // active new slide
+            timeSlides.forEach((slide, index) => {
+                if (index === selectedTime) { slide.classList.add('active') }
+            });
+        }
+        run()
+
+        this.setRealtimeWeather(weatherData[selectedTime])
     }
     // Set Time and Date
-    async setDateAndTime(){
-        // Access to user city
-        const userCity = localStorage.getItem('userCity');
-        // base url and api key
-        const url = 'https://api.api-ninjas.com/v1/worldtime?city=';
-        const key = 'UugH/GVejG63QV8L9voW/w==AEG2K0kCQF2h4aiG';
+    async setDateAndTime() {
 
-        // Get Date and time from Api
-        const response = fetch(url + userCity,{
-            headers: { 'X-Api-Key': key},
-            contentType: 'application/json',
-        }).then(res => {
-            return res.json()
-        }).catch(error => {
-            console.log(error);
-        })
-        // // Set date and time in Local storage
-        localStorage.setItem('time', JSON.stringify(await response) )
-    
-        const time = JSON.parse(localStorage.getItem('time'));
 
-        const {hour , minute , second, day_of_week, date} = await response;
-        
+        // Extract time from api
+        const time = await getApis.getTime();
+
+
         // access to HTML elemnts 
         const dateTag = document.querySelector('#time_and_date .date');
         const timeTag = document.querySelector('#time_and_date .time');
 
-        dateTag.innerHTML = `${day_of_week}    /    ${date}`;
-        timeTag.innerHTML = `${ hour }: ${minute} : ${second}`;
-    
+        dateTag.innerHTML = `${time.dayName}    /    ${time.date}`;
+        timeTag.innerHTML = `${time.watch.hour}: ${time.watch.minute} : ${time.watch.second}`;
+
         // run clock
-        this.runClock([hour, minute, second])
+        this.runClock([time.watch.hour, time.watch.minute, time.watch.second])
     }
     // Run clock
-    runClock(timeData){
+    runClock(timeData) {
         // access to the loading classes
         const loadingClasses = document.querySelectorAll(`#time_and_date .loading_frame`);
         // remove classes
         loadingClasses.forEach(currentElement => currentElement.classList.remove('loading_frame'))
         // access to the data
         let hour = Number(timeData[0]),
-        minute = Number(timeData[1]),
-        seconde = Number(timeData[2]);
+            minute = Number(timeData[1]),
+            seconde = Number(timeData[2]);
 
         // show values
         let showSeconde = '';
@@ -188,49 +164,96 @@ class WeatherApp {
         setInterval(() => {
             // seconde --------------
             seconde++
-            if(seconde > 59){
+            if (seconde > 59) {
                 seconde = 1;
             }
             // seconde fixer
-            if(seconde < 10){
+            if (seconde < 10) {
                 showSeconde = `0${seconde}`;
-            }else{
+            } else {
                 showSeconde = seconde;
             }
 
             // minute ------------------
-            if(seconde === 1){
+            if (seconde === 1) {
                 minute++
             }
-            if(minute > 59){
+            if (minute > 59) {
                 minute = 1
             }
             // minute fixer
-            if(minute < 10){
+            if (minute < 10) {
                 showMinute = `0${minute}`;
-            }else{
+            } else {
                 showMinute = minute;
             }
 
             // hour ---------------
-            if(minute === 1){
+            if (minute === 1) {
                 hour++
             }
 
             // hour fixer
-            if(hour < 10){
+            if (hour < 10) {
                 showHour = `0${hour}`;
-            }else{
+            } else {
                 showHour = hour;
             }
 
             timeTag.innerHTML = `${showHour} : ${showMinute} : ${showSeconde}`
         }, 1000);
 
-
     }
+    // Remove loading frame
+    removeLoadingFrame(elementId) {
+        // access to the loading classes
+        const loadingClasses = document.querySelectorAll(`#${elementId} .loading_frame`);
+        // remove classes
+        loadingClasses.forEach(currentElement => currentElement.classList.remove('loading_frame'))
+    }
+    // Run seconde city
+    async runSecondeCity(){
+        // access to seconde city from local storage
+        const userCity = localStorage.getItem('secondeCity').toLocaleLowerCase();
+
+        // show seconde city section
+        document.querySelector('#secondeCity .seconde-city-info').classList.remove('hidden');
+
+        // Access to seconde city weather
+        let weather = await getApis.getWeather(userCity);
+        weather = weather.list[0];
+        this.removeLoadingFrame('secondeCity');
+
+        // Set user City
+        const userCityTag = document.querySelector('#secondeCity .location')
+        userCityTag.innerHTML = userCity;
+
+        // Set description
+        const descriptionTag = document.querySelector('#secondeCity .description')
+        descriptionTag.innerHTML = weather.weather[0].description;
+
+        // Set weather temperature
+        const tempTag = document.querySelector('#secondeCity .temp')
+        tempTag.innerHTML = Math.floor(weather.main.temp);
+
+        // Set weather icon
+        const weatherImgTag = document.querySelector('#secondeCity .real-img img')
+        weatherImgTag.src =  await getApis.findIcon(weather);
+
+        // Set More info ------- access to elements
+        const moreInfo = document.querySelector('#secondeCity .more-weather'),
+            maxTempTag = moreInfo.querySelector('#max_temp h4'),
+            minTempTag = moreInfo.querySelector('#min_temp h4'),
+            humidityTag = moreInfo.querySelector('#humidity h4'),
+            windTag = moreInfo.querySelector('#wind h4');
 
 
+
+        maxTempTag.innerHTML = weather.main.temp_max + ' °';
+        minTempTag.innerHTML = weather.main.temp_min + ' °';
+        humidityTag.innerHTML = weather.main.humidity;
+        windTag.innerHTML = weather.wind.speed;
+    }
 
 }
 
